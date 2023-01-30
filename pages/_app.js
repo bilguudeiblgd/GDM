47b2ed930aa172242ecdd7dc6d22dcf83a898275
bilguudeiblgd <bilguudei.b8@gmail.com>
2023-01-30 20:34:53 +0100

    added router event

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import Router, {useRouter} from 'next/router';
 import NProgress from 'nprogress';
+import { useEffect } from 'react';
 import 'nprogress/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start()); 
@@ -9,12 +10,19 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   const handleRouteChange = (url) => {
     window.gtag('config', 'G-SDS6YY9HD1', {
       page_path: url,
     });
   };
 
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return <>
     <Layout>
       <Component {...pageProps} />
